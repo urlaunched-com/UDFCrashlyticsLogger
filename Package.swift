e23ae01f9724c7ca11eec841a4bb0563e123fae7
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2023-10-04 21:03:51 +0300

    ios 14

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "UDFCrashlyticsLogger",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v14),
         .macOS(.v11)
     ],
     products: [
