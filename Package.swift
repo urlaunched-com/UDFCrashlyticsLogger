3b444e58661a21b0094923277760e26bd61f4eff
Bogdan Petkanych <bogdanpetkanych@gmail.com>
2026-04-20 12:35:57 +0200

    Update firebase-ios-sdk dependencies

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Maks-Jago/SwiftUI-UDF", from: "1.5.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "12.0.0")
     ],
     targets: [
         .target(
